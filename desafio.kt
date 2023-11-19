939d11d283a4af8b62ed1dcc87c4a1365ace9339
Fabio Kusaba <118581190+fabiokusaba@users.noreply.github.com>
2023-11-19 16:20:09 -0300

    Resolução Desafio Orientação a Objetos com Kotlin

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,42 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-class Usuario
+data class Usuario(val name: String, val email: String, val password: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, val nivel: Nivel, val conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
     
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    fun matricular(vararg usuarios: Usuario) {
+        for(usuario in usuarios) {
+            inscritos.add(usuario)
+            println("Usuário ${usuario.name} matriculado(a) com sucesso.")
+        }    
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val nivel = Nivel.BASICO
+    
+    val usuario1 = Usuario("Maria", "maria@email.com", "maria123")
+    val usuario2 = Usuario("João", "joão@email.com", "joao1234")
+    val usuario3 = Usuario("Pedro", "pedro@email.com", "1234567")
+    
+    val kotlinIntroducao = ConteudoEducacional("Kotlin Introdução", 2)
+    val kotlinFuncoes = ConteudoEducacional("Kotlin Funções", 2)
+    val kotlinClasses = ConteudoEducacional("Kotlin Classes", 2)
+    val kotlinComExemplos = ConteudoEducacional("Kotlin com exemplos", 2)
+    
+    val listaConteudosEducacionais = listOf(kotlinIntroducao, kotlinFuncoes, kotlinClasses, kotlinComExemplos)
+    
+    val bootcampKotlin = Formacao("NTT Data Bootcamp com Kotlin", nivel, listaConteudosEducacionais)
+    
+    println(bootcampKotlin)
+    
+    bootcampKotlin.matricular(usuario1, usuario2, usuario3)
+    
+    println(bootcampKotlin.inscritos)
 }
